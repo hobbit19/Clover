e82bf848de66b9582c74516b7ab7d1c4d3425139
Alex James <theracermaster@gmail.com>
2016-12-24 14:10:57 -0600

    Clover: Embed protocol implementations into Clover
    Currently, mandatory UEFI drivers (such as FSInject) must be manually
    copied to the appropriate folder (e.g. drivers64uefi). Instead of
    relying on mandatory drivers loaded from the driver folders, embed the
    AppleGraphicsConfiguration, EfiOSInfo, FSInject, FvInject, SmcHelper
    and UserInterfaceThemeDriver implementations in Clover itself.

--- rEFIt_UEFI/refit/main.c
+++ rEFIt_UEFI/refit/main.c
@@ -2169,6 +2169,7 @@ RefitMain (IN EFI_HANDLE           ImageHandle,
   //DBG("ScanSPD() end\n");
 
   SetPrivateVarProto();
+  InstallAppleProtocols ();
 //  GetDefaultSettings();
   GetAcpiTablesList();
 
